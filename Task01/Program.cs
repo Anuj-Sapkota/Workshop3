b7bb13367ef4bbfbe2f260dcd3a22e4e910eae09
Anuj-sapkota <anujsapkota808@gmail.com>
2025-11-10 11:25:37 +0545

    Task 01 completed

---
+++ Task01/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Task01.Operators op = new Task01.Operators();
+        op.Add(1, 2);
+        op.Subtract(2,1);
+        op.Multiply(3,4);
+        op.Divide(10,5);
+    }
+}
