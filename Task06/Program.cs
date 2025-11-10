3dc1696b76f84b7927ce45010b65d3af1a927f80
Anuj-sapkota <anujsapkota808@gmail.com>
2025-11-10 12:12:22 +0545

    Task 06 completed

---
+++ Task06/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+    public class Program
+    {
+        public static void Main()
+        {
+            // Console.Write("Enter a number: ");
+            // string input = Console.ReadLine();
+            //
+            // try
+            // {
+            //     int number = int.Parse(input!); 
+            //     Console.WriteLine("You entered: " + number);
+            // }
+            // catch (Exception)
+            // {
+            //     Console.WriteLine("Invalid number format");
+            // }
+            // finally
+            // {
+            //     Console.WriteLine("Program Executed");
+            // }
+            
+            Console.Write("Enter your password: ");
+            string password = Console.ReadLine();
+
+            try
+            {
+                if (password.Length < 6)
+                {
+                    throw new Exception("Password must be at least 6 characters.");
+                }
+
+                Console.WriteLine("Password satisfied the requirement");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
