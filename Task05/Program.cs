63579d281225a4c1b61a775af666fb08e822bdb0
Anuj-sapkota <anujsapkota808@gmail.com>
2025-11-10 12:05:54 +0545

    Task 05 completed

---
+++ Task05/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+    public class Program
+    {
+        public static void Main()
+        {
+           
+            // Console.Write("Enter a number N: ");
+            // string input = Console.ReadLine();
+            //
+            // if (int.TryParse(input, out int N) && N > 0)
+            // {
+            //     int sum = 0;
+            //     for (int i = 1; i <= N; i++)
+            //     {
+            //         sum += i;
+            //     }
+            //     Console.WriteLine($"Sum from 1 to {N} is: {sum}");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Invalid input! Enter a positive integer.");
+            // }
+            
+            int i = 1;
+            while (i <= 20)
+            {
+                if (i % 4 == 0)
+                {
+                    i++;
+                    continue; 
+                }
+
+                if (i == 15)
+                {
+                    break; 
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+
